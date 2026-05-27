c584feb30016f8c0411acf9a7d317ba81fd38056
William Sousa <williamrsousa94@hotmail.com>
2026-05-27 16:33:26 -0300

    Project started

---
+++ src/main/java/com/willrsousa/crudchallenge/CrudchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.willrsousa.crudchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudchallengeApplication.class, args);
+	}
+
+}
